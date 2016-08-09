f741a576020fee61d9417e881803c91bc32f8ae1
zeroRooter <williamap@gmail.com>
2016-08-09 03:03:48 -0400

    add _handle_response method in quadrigacx

--- quadrigacx.py
+++ quadrigacx.py
@@ -22,114 +22,81 @@ class Quadriga:
     def get_account_balance(self):
         signature, nonce = self.generate_signature()
         response = requests.post('https://api.quadrigacx.com/v2/balance', data={'key':self.apiKey,'signature':signature,'nonce':nonce})
-        if response.status_code==200:
-            return response.json()
-        else:
-            return {'error':'code: '+str(response.status_code)}
-
+        self._handle_response(self, response)
+        
     def get_user_transactions(self, offset=0, limit=100, sort='desc', book='btc_cad'):
         signature, nonce = self.generate_signature()
         response = requests.post('https://api.quadrigacx.com/v2/user_transactions',
                                  data={'key': self.apiKey, 'signature': signature, 'nonce': nonce, 'offset':offset,'limit':limit,'sort':sort,'book':book})
-        if response.status_code == 200:
-            return response.json()
-        else:
-            return {'error': 'code: ' + str(response.status_code)}
+        self._handle_response(self, response)
 
     def get_open_orders(self, book='btc_cad'):
         signature, nonce = self.generate_signature()
         response = requests.post('https://api.quadrigacx.com/v2/open_orders',
                                  data={'key': self.apiKey, 'signature': signature, 'nonce': nonce, 'book': book})
-        if response.status_code == 200:
-            return response.json()
-        else:
-            return {'error': 'code: ' + str(response.status_code)}
+        self._handle_response(self, response)
 
     def lookup_order(self, id = ''):
         signature, nonce = self.generate_signature()
         response = requests.post('https://api.quadrigacx.com/v2/lookup_order',
                                  data={'key': self.apiKey, 'signature': signature, 'nonce': nonce, 'id': id})
-        if response.status_code == 200:
-            return response.json()
-        else:
-            return {'error': 'code: ' + str(response.status_code)}
+        self._handle_response(self, response)
 
     def cancel_order(self, id = ''):
         signature, nonce = self.generate_signature()
         response = requests.post('https://api.quadrigacx.com/v2/cancel_order',
                                  data={'key': self.apiKey, 'signature': signature, 'nonce': nonce, 'id': id})
-        if response.status_code == 200:
-            return response.text
-        else:
-            return {'error': 'code: ' + str(response.status_code)}
+        self._handle_response(self, response)
 
     def buy_order_limit(self, amount, price, book='btc_cad'):
         signature, nonce = self.generate_signature()
         response = requests.post('https://api.quadrigacx.com/v2/buy',
                                  data={'key': self.apiKey, 'signature': signature, 'nonce': nonce, 'amount':amount, 'price':price, 'book':book})
-        if response.status_code == 200:
-            return response.json()
-        else:
-            return {'error': 'code: ' + str(response.status_code)}
+        self._handle_response(self, response)
 
     def buy_order_market(self, amount, book='btc_cad'):
         signature, nonce = self.generate_signature()
         response = requests.post('https://api.quadrigacx.com/v2/buy',
                                  data={'key': self.apiKey, 'signature': signature, 'nonce': nonce, 'amount':amount, 'book':book})
-        if response.status_code == 200:
-            return response.json()
-        else:
-            return {'error': 'code: ' + str(response.status_code)}
+        self._handle_response(self, response)
 
     def sell_order_limit(self, amount, price, book='btc_cad'):
         signature, nonce = self.generate_signature()
         response = requests.post('https://api.quadrigacx.com/v2/sell',
                                  data={'key': self.apiKey, 'signature': signature, 'nonce': nonce, 'amount':amount, 'price':price, 'book':book})
-        if response.status_code == 200:
-            return response.json()
-        else:
-            return {'error': 'code: ' + str(response.status_code)}
+        self._handle_response(self, response)
 
     def sell_order_market(self, amount, book='btc_cad'):
         signature, nonce = self.generate_signature()
         response = requests.post('https://api.quadrigacx.com/v2/sell',
                                  data={'key': self.apiKey, 'signature': signature, 'nonce': nonce, 'amount':amount, 'book':book})
-        if response.status_code == 200:
-            return response.json()
-        else:
-            return {'error': 'code: ' + str(response.status_code)}
+        self._handle_response(self, response)
 
     def bitcoin_deposit(self):
         signature, nonce = self.generate_signature()
         response = requests.post('https://api.quadrigacx.com/v2/bitcoin_deposit_address',
                                  data={'key': self.apiKey, 'signature': signature, 'nonce': nonce})
-        if response.status_code == 200:
-            return response.text
-        else:
-            return {'error': 'code: ' + str(response.status_code)}
+        self._handle_response(self, response)
 
     def bitcoin_withdrawal(self, amount, address):
         signature, nonce = self.generate_signature()
         response = requests.post('https://api.quadrigacx.com/v2/bitcoin_withdrawal',
                                  data={'key': self.apiKey, 'signature': signature, 'nonce': nonce, 'amount':amount, 'address':address})
-        if response.status_code == 200:
-            return response.text
-        else:
-            return {'error': 'code: ' + str(response.status_code)}
+        self._handle_response(self, response)
 
     def ether_deposit(self):
         signature, nonce = self.generate_signature()
         response = requests.post('https://api.quadrigacx.com/v2/ether_deposit_address',
                                  data={'key': self.apiKey, 'signature': signature, 'nonce': nonce})
-        if response.status_code == 200:
-            return response.text
-        else:
-            return {'error': 'code: ' + str(response.status_code)}
+        self._handle_response(self, response)
 
     def ether_withdrawal(self, amount, address):
         signature, nonce = self.generate_signature()
         response = requests.post('https://api.quadrigacx.com/v2/ether_withdrawal',
                                  data={'key': self.apiKey, 'signature': signature, 'nonce': nonce, 'amount':amount, 'address':address})
+        self._handle_response(self, response)
+
+    def _handle_response(self, response):
         if response.status_code == 200:
             return response.text
         else:
