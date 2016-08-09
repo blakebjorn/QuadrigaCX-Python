9a3a9c70c7d7d173d1e176977b77fbfee1e44c2a
zeroRooter <williamap@gmail.com>
2016-08-09 02:42:54 -0400

    added dummy unit tests

---
+++ quadrigacxtest.py
@@ -0,0 +1,50 @@
+import quadrigacx
+import unittest
+
+class TestQuadrigaCx(unittest.TestCase):
+    def test_generate_signature(self):
+        self.assertEqual(True, True) #dummy test
+
+    def test_get_account_balance(self):
+        self.assertEqual(True, True) #dummy test
+
+    def test_get_user_transactions(self):
+        self.assertEqual(True, True) #dummy test
+
+    def test_get_open_orders(self):
+        self.assertEqual(True, True) #dummy test
+
+    def test_lookup_order(self):
+        self.assertEqual(True, True) #dummy test
+
+    def test_cancel_order(self):
+        self.assertEqual(True, True) #dummy test
+
+    def test_buy_order_limit(self):
+        self.assertEqual(True, True) #dummy test
+
+    def test_buy_order_market(self):
+        self.assertEqual(True, True) #dummy test
+
+    def test_sell_order_limit(self):
+        self.assertEqual(True, True) #dummy test
+
+    def test_sell_order_market(self):
+        self.assertEqual(True, True) #dummy test
+
+    def test_bitcoin_deposit(self):
+        self.assertEqual(True, True) #dummy test
+
+    def test_bitcoin_withdrawal(self):
+        self.assertEqual(True, True) #dummy test
+
+    def test_ether_deposit(self):
+        self.assertEqual(True, True) #dummy test
+
+    def test_ether_withdrawal(self):
+        self.assertEqual(True, True) #dummy test
+
+
+
+if __name__ == '__main__':
+    unittest.main()
