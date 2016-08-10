b98614e26e0d90da477509a3be84d5e6b4bfc2c2
blakebjorn <blakebanders@gmail.com>
2016-08-10 10:14:48 -0400

    renamed function 'get_all_open_orders' to 'get_order_book', constructor argument to allow string returns instead of dictionaries. Removed unused import

--- quadrigacx.py
+++ quadrigacx.py
@@ -2,13 +2,14 @@ import requests
 import time
 import hmac
 import hashlib
-from array import array
 
 class Quadriga:
-    def __init__(self, apiKey='string', apiSecret='string', clientID='integer'):
+    # Setting parseDicts to False returns all responses as strings, rather than parsing them as dictionaries when json responses are possible
+    def __init__(self, apiKey='string', apiSecret='string', clientID='integer', parseDicts=True):
         self.apiKey = apiKey
         self.apiSecret = apiSecret
         self.clientID = clientID
+        self.parseDicts = parseDicts # Return json data as a dictionary. False -> string responses
 
     def generate_signature(self):
         #uses ms for nonce to avoid collision
@@ -23,97 +24,99 @@ class Quadriga:
     def get_account_balance(self):
         signature, nonce = self.generate_signature()
         response = requests.post('https://api.quadrigacx.com/v2/balance', data={'key':self.apiKey,'signature':signature,'nonce':nonce})
-        return self._handle_response(response)
+        return self._handle_response(response, parse = self.parseDicts)
 
     def get_user_transactions(self, offset=0, limit=100, sort='desc', book='btc_cad'):
         signature, nonce = self.generate_signature()
         response = requests.post('https://api.quadrigacx.com/v2/user_transactions',
                                  data={'key': self.apiKey, 'signature': signature, 'nonce': nonce, 'offset':offset,'limit':limit,'sort':sort,'book':book})
-        return self._handle_response(response)
+        return self._handle_response(response, parse = self.parseDicts)
 
-    def get_all_open_orders(self):
+    def get_order_book(self):
         signature, nonce = self.generate_signature()
         response = requests.post('https://api.quadrigacx.com/v2/order_book', data={'key':self.apiKey,'signature':signature,'nonce':nonce})
-        return self._handle_response(response)
+        return self._handle_response(response, parse = self.parseDicts)
 
     def get_current_trading_info(self):
         signature, nonce = self.generate_signature()
         response = requests.post('https://api.quadrigacx.com/v2/ticker', data={'key':self.apiKey,'signature':signature,'nonce':nonce})
-        return self._handle_response(response)
-
-
-
-
+        return self._handle_response(response, parse = self.parseDicts)
 
     def get_open_orders(self, book='btc_cad'):
         signature, nonce = self.generate_signature()
         response = requests.post('https://api.quadrigacx.com/v2/open_orders',
                                  data={'key': self.apiKey, 'signature': signature, 'nonce': nonce, 'book': book})
-        return self._handle_response(response)
+        return self._handle_response(response, parse = self.parseDicts)
 
     def lookup_order(self, id = ''):
         signature, nonce = self.generate_signature()
         response = requests.post('https://api.quadrigacx.com/v2/lookup_order',
                                  data={'key': self.apiKey, 'signature': signature, 'nonce': nonce, 'id': id})
-        return self._handle_response(response)
+        return self._handle_response(response, parse = self.parseDicts)
 
     def cancel_order(self, id = ''):
         signature, nonce = self.generate_signature()
         response = requests.post('https://api.quadrigacx.com/v2/cancel_order',
                                  data={'key': self.apiKey, 'signature': signature, 'nonce': nonce, 'id': id})
-        return self._handle_response(response)
+        return self._handle_response(response, parse = False)
 
     def buy_order_limit(self, amount, price, book='btc_cad'):
         signature, nonce = self.generate_signature()
         response = requests.post('https://api.quadrigacx.com/v2/buy',
                                  data={'key': self.apiKey, 'signature': signature, 'nonce': nonce, 'amount':amount, 'price':price, 'book':book})
-        return self._handle_response(response)
+        return self._handle_response(response, parse = self.parseDicts)
 
     def buy_order_market(self, amount, book='btc_cad'):
         signature, nonce = self.generate_signature()
         response = requests.post('https://api.quadrigacx.com/v2/buy',
                                  data={'key': self.apiKey, 'signature': signature, 'nonce': nonce, 'amount':amount, 'book':book})
-        return self._handle_response(response)
+        return self._handle_response(response, parse = self.parseDicts)
 
     def sell_order_limit(self, amount, price, book='btc_cad'):
         signature, nonce = self.generate_signature()
         response = requests.post('https://api.quadrigacx.com/v2/sell',
                                  data={'key': self.apiKey, 'signature': signature, 'nonce': nonce, 'amount':amount, 'price':price, 'book':book})
-        return self._handle_response(response)
+        return self._handle_response(response, parse = self.parseDicts)
 
     def sell_order_market(self, amount, book='btc_cad'):
         signature, nonce = self.generate_signature()
         response = requests.post('https://api.quadrigacx.com/v2/sell',
                                  data={'key': self.apiKey, 'signature': signature, 'nonce': nonce, 'amount':amount, 'book':book})
-        return self._handle_response(response)
+        return self._handle_response(response, parse = self.parseDicts)
 
     def bitcoin_deposit(self):
         signature, nonce = self.generate_signature()
         response = requests.post('https://api.quadrigacx.com/v2/bitcoin_deposit_address',
                                  data={'key': self.apiKey, 'signature': signature, 'nonce': nonce})
-        return self._handle_response(response)
+        return self._handle_response(response, parse = False)
 
     def bitcoin_withdrawal(self, amount, address):
         signature, nonce = self.generate_signature()
         response = requests.post('https://api.quadrigacx.com/v2/bitcoin_withdrawal',
                                  data={'key': self.apiKey, 'signature': signature, 'nonce': nonce, 'amount':amount, 'address':address})
-        return self._handle_response(response)
+        return self._handle_response(response, parse = False)
 
     def ether_deposit(self):
         signature, nonce = self.generate_signature()
         response = requests.post('https://api.quadrigacx.com/v2/ether_deposit_address',
                                  data={'key': self.apiKey, 'signature': signature, 'nonce': nonce})
-        return self._handle_response(response)
+        return self._handle_response(response, parse = False)
 
     def ether_withdrawal(self, amount, address):
         signature, nonce = self.generate_signature()
         response = requests.post('https://api.quadrigacx.com/v2/ether_withdrawal',
                                  data={'key': self.apiKey, 'signature': signature, 'nonce': nonce, 'amount':amount, 'address':address})
-        return self._handle_response(response)
+        return self._handle_response(response, parse = False)
 
-    def _handle_response(self, response):
+    def _handle_response(self, response, parse = False):
         if response.status_code == 200:
-            return response.json()
+            if parse:
+                return response.json()
+            else:
+                return response.text
         else:
             print('error '+ ' code: ' + str(response.status_code))
-            return {'error': 'code: ' + str(response.status_code)}
+            if parse:
+                return {'error': 'code: ' + str(response.status_code)}
+            else:
+                return 'error - code: ' + str(response.status_code)
